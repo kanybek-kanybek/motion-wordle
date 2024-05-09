577a5e53d7a41fe988219c6e6355efa7586104a0
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-05-09 23:02:45 +0600

    UI jasadym

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Block from "./components/block";
-import UseFetch from "./components/api/useFetch";
 
 function App() {
     return (
         <>
             <Block />
-            <UseFetch />
         </>
     );
 }
--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "../components/block.css";
 function Block() {
     return (
