65ea787498ed2128843c8f6aca7b5e9b9410a3a3
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-05-13 22:15:14 +0600

    logika jazuu

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,76 +1,64 @@
-import "../components/block.css";
+import React, { useState } from "react";
+import "./block.css";
 
-const API_URL = "https://piccolo-server.vercel.app/words";
+interface BoardProps {
+    tiles: string[];
+    onTileChange: (index: number, value: string) => void;
+}
 
-fetch(API_URL)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then((data) => {
-        const outputElement = document.createElement("h1");
-        outputElement.textContent = JSON.stringify(data);
+const Block: React.FC<BoardProps> = ({ tiles, onTileChange }) => {
+    const firstFiveTiles = tiles.slice(0, 25);
+    const [work, setWork] = useState("");
 
-        document.body.appendChild(outputElement);
-    })
-    .catch((error) => {
-        console.error(error.find(() => error.data));
-    });
+    // console.log(onTileChange);
+    // function Handler(tiles:string) {
+    //     if(tiles === ) {
 
-function Block() {
+    //     }
+    // }
+    console.log(work);
+
     return (
-        <>
-            <div className="wrap">
-                <div className="container">
-                    <div className="block">
-                        <div className="block__content">
-                            <div className="block__content__inputs">
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                            </div>
-                            <div className="block__content__inputs">
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                            </div>
-                            <div className="block__content__inputs">
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                            </div>
-                            <div className="block__content__inputs">
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                            </div>
-                            <div className="block__content__inputs">
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                                <input type="text" name="" id="" />
-                            </div>
+        <div id="cube">
+            <div className="container">
+                <input
+                    style={{
+                        width: "400px",
+                        height: "50px",
+                        marginLeft: "50px",
+                        borderRadius: "8px",
+                        justifyContent: "center",
+                        paddingLeft: "10px",
+                    }}
+                    type="text"
+                    placeholder="text"
+                    value={work}
+                    onChange={(e) => setWork(e.target.value)}
+                />
+                <div className="block">
+                    <div className="block__content">
+                        <div className="block__content__inputs">
+                            {firstFiveTiles.map((letter, index) => (
+                                <input
+                                    key={index}
+                                    type="text"
+                                    value={letter}
+                                    maxLength={1}
+                                    onChange={(e) =>
+                                        onTileChange(index, e.target.value)
+                                    }
+                                />
+                            ))}
                         </div>
-                        <div className="block__title">
-                            <p>Wordle #195</p>
-                            <p>12/31/2021</p>
-                        </div>
+                    </div>
+                    <div className="wordle__text">
+                        <p>Wordle #195</p>
+                        <p>12/31/2021</p>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
-}
+};
 
 export default Block;
