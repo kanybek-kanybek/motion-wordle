732cf03a48d3fe06523c0426c7cbe7353e99661b
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-05-19 15:29:56 +0600

    resetGame and alertwordl

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,30 +1,22 @@
 import React, { useState, useRef } from "react";
 import "../components/block.css";
 import Keyboard from "./Keyboard/Keyboard";
-
+import { BsArrowClockwise } from "react-icons/bs";
 interface BoardProps {
     tile: string;
     loading: boolean;
 }
 
 const Block: React.FC<BoardProps> = ({ tile, loading }) => {
-    const [inputValuesOne, setInputValuesOne] = useState<string[]>(
-        Array(5).fill("")
-    );
-    const [inputValuesTwo, setInputValuesTwo] = useState<string[]>(
-        Array(5).fill("")
-    );
-    const [inputValuesThree, setInputValuesThree] = useState<string[]>(
-        Array(5).fill("")
-    );
-    const [inputValuesFour, setInputValuesFour] = useState<string[]>(
-        Array(5).fill("")
-    );
-    const [inputValuesFive, setInputValuesFive] = useState<string[]>(
-        Array(5).fill("")
+    const [inputValues, setInputValues] = useState<string[][]>(
+        Array(5)
+            .fill("")
+            .map(() => Array(5).fill(""))
     );
     const [inputColors, setInputColors] = useState<string[][]>(
-        Array(5).fill(Array(5).fill("white"))
+        Array(5)
+            .fill("")
+            .map(() => Array(5).fill("white"))
     );
     const [invalidInputs, setInvalidInputs] = useState<{
         [key: string]: boolean;
@@ -32,45 +24,27 @@ const Block: React.FC<BoardProps> = ({ tile, loading }) => {
     const [currentRow, setCurrentRow] = useState<number>(0);
     const [currentCol, setCurrentCol] = useState<number>(0);
 
-    const inputRefsOne = useRef<(HTMLInputElement | null)[]>(
-        Array(5).fill(null)
-    );
-    const inputRefsTwo = useRef<(HTMLInputElement | null)[]>(
-        Array(5).fill(null)
-    );
-    const inputRefsThree = useRef<(HTMLInputElement | null)[]>(
-        Array(5).fill(null)
-    );
-    const inputRefsFour = useRef<(HTMLInputElement | null)[]>(
-        Array(5).fill(null)
-    );
-    const inputRefsFive = useRef<(HTMLInputElement | null)[]>(
-        Array(5).fill(null)
+    const inputRefs = useRef<(HTMLInputElement | null)[][]>(
+        Array(5)
+            .fill("")
+            .map(() => Array(5).fill(null))
     );
 
-    const allInputValues = [
-        inputValuesOne,
-        inputValuesTwo,
-        inputValuesThree,
-        inputValuesFour,
-        inputValuesFive,
-    ];
-
-    const setInputValuesFunctions = [
-        setInputValuesOne,
-        setInputValuesTwo,
-        setInputValuesThree,
-        setInputValuesFour,
-        setInputValuesFive,
-    ];
-
-    const inputRefs = [
-        inputRefsOne,
-        inputRefsTwo,
-        inputRefsThree,
-        inputRefsFour,
-        inputRefsFive,
-    ];
+    const resetGame = () => {
+        setInputValues(
+            Array(5)
+                .fill("")
+                .map(() => Array(5).fill(""))
+        );
+        setInputColors(
+            Array(5)
+                .fill("")
+                .map(() => Array(5).fill("white"))
+        );
+        setInvalidInputs({});
+        setCurrentRow(0);
+        setCurrentCol(0);
+    };
 
     const handleSave = () => {
         const newColors = inputColors.slice();
@@ -78,12 +52,12 @@ const Block: React.FC<BoardProps> = ({ tile, loading }) => {
 
         let anyCorrect = false;
 
-        allInputValues.forEach((inputValues, rowIndex) => {
-            let rowString = inputValues.join("");
+        inputValues.forEach((inputRow, rowIndex) => {
+            let rowString = inputRow.join("");
             let tileChars = tile.split("");
             let inputColorsRow = Array(5).fill("white");
 
-            inputValues.forEach((value, colIndex) => {
+            inputRow.forEach((value, colIndex) => {
                 if (!value || value.trim() === "") {
                     newInvalidInputs[`${rowIndex}-${colIndex}`] = true;
                 } else {
@@ -94,7 +68,7 @@ const Block: React.FC<BoardProps> = ({ tile, loading }) => {
                 }
             });
 
-            inputValues.forEach((value, colIndex) => {
+            inputRow.forEach((value, colIndex) => {
                 if (
                     value &&
                     value !== tile[colIndex] &&
@@ -116,115 +90,114 @@ const Block: React.FC<BoardProps> = ({ tile, loading }) => {
         setInputColors(newColors);
 
         if (anyCorrect) {
-            alert("Сиз жеңдиңиз!");
+            alert("Сиз жеңдиңиз!Куттуктаймын!");
+            resetGame();
         } else if (currentRow === 4) {
-            alert("Сиз жеңбей калдыңыз, кайра ойноп көрүңүз.");
+            alert(
+                `Сиз туура таба албай калдыңыз, кайра ойноп көрүңүз! Сиз таба албай жаткан сөз: ${tile}`
+            );
+            resetGame();
+        } else {
+            setCurrentRow(currentRow + 1);
+            setCurrentCol(0);
         }
     };
 
     const handleChange = (
-        index: number,
-        value: string,
-        setInputValues: React.Dispatch<React.SetStateAction<string[]>>,
-        inputRefs: React.RefObject<(HTMLInputElement | null)[]>
+        rowIndex: number,
+        colIndex: number,
+        value: string
     ) => {
         setInputValues((prevValues) => {
-            const newInputValues = [...prevValues];
-            newInputValues[index] = value;
+            const newValues = prevValues.map((row, i) =>
+                row.map((col, j) =>
+                    i === rowIndex && j === colIndex ? value : col
+                )
+            );
 
-            let nextIndex = index;
-            if (value.length === 1 && index < 4) {
-                nextIndex = index + 1;
-            } else if (value.length === 0 && index > 0) {
-                nextIndex = index - 1;
+            let nextColIndex = colIndex;
+            if (value.length === 1 && colIndex < 4) {
+                nextColIndex = colIndex + 1;
+            } else if (value.length === 0 && colIndex > 0) {
+                nextColIndex = colIndex - 1;
             }
 
-            if (nextIndex >= 0 && nextIndex < 5 && inputRefs.current) {
-                inputRefs.current[nextIndex]?.focus();
+            setCurrentCol(nextColIndex);
+
+            if (inputRefs.current[rowIndex][nextColIndex]) {
+                inputRefs.current[rowIndex][nextColIndex]?.focus();
             }
 
-            return newInputValues;
+            return newValues;
         });
 
-        const key = `${inputRefs.current?.indexOf(
-            inputRefs.current[index]
-        )}-${index}`;
         setInvalidInputs((prev) => {
             const newInvalidInputs = { ...prev };
-            delete newInvalidInputs[key];
+            delete newInvalidInputs[`${rowIndex}-${colIndex}`];
             return newInvalidInputs;
         });
     };
 
     const handleKeyPress = (key: string) => {
-        const currentInputValues = allInputValues[currentRow];
-        const setCurrentInputValues = setInputValuesFunctions[currentRow];
-        const currentInputRefs = inputRefs[currentRow];
-
         if (key === "BACKSPACE") {
-            const newValues = [...currentInputValues];
-            newValues[currentCol - 1] = "";
-            setCurrentInputValues(newValues);
-            setCurrentCol(Math.max(0, currentCol - 1));
-            if (currentInputRefs.current[currentCol - 1]) {
-                currentInputRefs.current[currentCol - 1]?.focus();
+            if (currentCol > 0) {
+                handleChange(currentRow, currentCol - 1, "");
             }
         } else if (key === "ENTER") {
             if (currentCol === 5) {
                 handleSave();
-                setCurrentRow((prev) => prev + 1);
-                setCurrentCol(0);
             }
         } else if (currentCol < 5) {
-            const newValues = [...currentInputValues];
-            newValues[currentCol] = key;
-            setCurrentInputValues(newValues);
-            setCurrentCol(currentCol + 1);
-            if (currentInputRefs.current[currentCol]) {
-                currentInputRefs.current[currentCol]?.focus();
-            }
+            handleChange(currentRow, currentCol, key);
         }
     };
 
     return (
         <div id="cube">
             <div className="container">
-                <h1>{tile}</h1>
+                <button
+                    style={{
+                        padding: "18px 40px",
+                        borderRadius: "15px",
+                        marginLeft: "590px",
+                        display: "flex",
+                        marginTop: "20px",
+                    }}
+                    onClick={resetGame}
+                >
+                    <BsArrowClockwise />
+                </button>
                 <div className="block">
                     <div className="block__content">
                         <div className="block__content__inputs">
-                            {allInputValues.map((inputValues, rowIndex) => (
+                            {inputValues.map((inputRow, rowIndex) => (
                                 <div key={`group-${rowIndex}`}>
-                                    {inputValues.map((value, colIndex) => {
-                                        const key = `${rowIndex}-${colIndex}`;
-                                        const backgroundColor =
-                                            inputColors[rowIndex][colIndex];
-
-                                        return (
-                                            <input
-                                                key={`input-${rowIndex}-${colIndex}`}
-                                                type="text"
-                                                value={value}
-                                                maxLength={1}
-                                                ref={(el) => {
-                                                    inputRefs[rowIndex].current[
+                                    {inputRow.map((value, colIndex) => (
+                                        <input
+                                            key={`input-${rowIndex}-${colIndex}`}
+                                            type="text"
+                                            value={value}
+                                            maxLength={1}
+                                            ref={(el) => {
+                                                inputRefs.current[rowIndex][
+                                                    colIndex
+                                                ] = el;
+                                            }}
+                                            onChange={(e) =>
+                                                handleChange(
+                                                    rowIndex,
+                                                    colIndex,
+                                                    e.target.value
+                                                )
+                                            }
+                                            style={{
+                                                backgroundColor:
+                                                    inputColors[rowIndex][
                                                         colIndex
-                                                    ] = el;
-                                                }}
-                                                onChange={(e) =>
-                                                    handleChange(
-                                                        colIndex,
-                                                        e.target.value,
-                                                        setInputValuesFunctions[
-                                                            rowIndex
-                                                        ],
-                                                        inputRefs[rowIndex]
-                                                    )
-                                                }
-                                                style={{ backgroundColor }}
-                                            />
-                                        );
-                                    })}
+                                                    ],
+                                            }}
+                                        />
+                                    ))}
                                 </div>
                             ))}
                         </div>
